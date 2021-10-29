27bd84d5b0c13c19b80a380a67e9518558fa2992
nancynobody <mck.workman@gmail.com>
2021-10-29 18:18:47 -0400

    updates

--- __test__/store.db.test.ts
+++ __test__/store.db.test.ts
@@ -1,1 +1,19 @@
-// TODO
+import mongoose from "mongoose";
+import { Store } from "../src/models";
+import { expect } from "chai";
+
+describe("store", () => {
+  before(async () => {
+    await mongoose.connect("mongodb://127.0.0.1:27017/test");
+  });
+
+  after(async () => {
+    await mongoose.disconnect();
+  });
+
+  it("should not create new account without proper params", async () => {});
+  it("should add a new account", async () => {});
+  it("should update existing account", async () => {});
+  it("should get most risky accounts", () => {});
+  it("should get least risky accounts", () => {});
+});
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,5 +1,6 @@
-import { Account } from "./account";
+import { Store } from "./store";
 import { Event } from "./event";
+import { Account } from "./account";
 // import { Market } from "./market";
 
-export { Account, Event };
+export { Store, Event, Account };
